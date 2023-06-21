706be652cbc902f08c7606e5528fbc5614c03527
Christopher Neisen <christopherjay71186@gmail.com>
2023-06-21 14:42:50 -0500

    catptcha fixed, now sendgrid wont work, troubleshooting sendgrid on live

--- src/pages/api/sendgrid.js
+++ src/pages/api/sendgrid.js
@@ -23,25 +23,32 @@ function sendEmail(req, res) {
 
       if (reCaptchaRes.success && reCaptchaRes.score >= 0.5) {
         console.log("I'm about to send an email");
-        sendgrid.send({
-          to: "chris@integritytechsoftware.com", // Your email where you'll receive emails
-          from: "Christopherjay71186@gmail.com", // your website email address here
-          subject: `New Contact Message`,
-          html: `<div><p>Full Name: ${req.body.fullname}</p>
+        sendgrid
+          .send({
+            to: "chris@integritytechsoftware.com", // Your email where you'll receive emails
+            from: "Christopherjay71186@gmail.com", // your website email address here
+            subject: `New Contact Message`,
+            html: `<div><p>Full Name: ${req.body.fullname}</p>
       <p>Email: ${req.body.email}</p>
       <p>Contact Reason: ${req.body.reason}</p>
       <p>Message: ${req.body.message}</p></div>`,
-        });
-        console.log("email sent");
-        console.log("I'm sending the success code");
-        return res.status(200);
+          })
+          .then(() => {
+            console.log("email sent");
+            console.log("I'm sending the success code");
+            return res.status(200);
+          })
+          .catch((e) => {
+            console.log("something went wrong", e);
+            return res.status(500);
+          });
       } else {
         console.log("recaptchares", reCaptchaRes);
         return res.status(401);
       }
     })
-    .catch((error) => {
-      console.log("something went wrong", error);
+    .catch((e) => {
+      console.log("something went wrong", e);
       return res.status(500);
     });
 }
