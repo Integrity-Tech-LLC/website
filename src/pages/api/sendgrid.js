c5f977a2da59c5d9927920b62e4b969ad1c2cb91
Christopher Neisen <christopherjay71186@gmail.com>
2023-06-21 14:16:47 -0500

    still not working on live

--- src/pages/api/sendgrid.js
+++ src/pages/api/sendgrid.js
@@ -14,39 +14,38 @@ const verifyRecaptcha = async (token) => {
   });
 };
 
-async function sendEmail(req, res) {
+function sendEmail(req, res) {
   console.log("I'm inside send email");
   verifyRecaptcha(req.body.token)
     .then((reCaptchaRes) => reCaptchaRes.json())
     .then((reCaptchaRes) => {
       console.log("captcha is verified");
-      try {
-        if (reCaptchaRes.success && reCaptchaRes.score >= 0.5) {
-          console.log("I'm about to send an email");
-          sendgrid.send({
-            to: "chris@integritytechsoftware.com", // Your email where you'll receive emails
-            from: "Christopherjay71186@gmail.com", // your website email address here
-            subject: `New Contact Message`,
-            html: `<div><p>Full Name: ${req.body.fullname}</p>
+
+      if (reCaptchaRes.success && reCaptchaRes.score >= 0.5) {
+        console.log("I'm about to send an email");
+        sendgrid.send({
+          to: "chris@integritytechsoftware.com", // Your email where you'll receive emails
+          from: "Christopherjay71186@gmail.com", // your website email address here
+          subject: `New Contact Message`,
+          html: `<div><p>Full Name: ${req.body.fullname}</p>
       <p>Email: ${req.body.email}</p>
       <p>Contact Reason: ${req.body.reason}</p>
       <p>Message: ${req.body.message}</p></div>`,
-          });
-          console.log("email sent");
-        } else {
-          console.log("recaptchares", reCaptchaRes);
-          return res
-            .status(error.statusCode || 401)
-            .json({ error: "Captcha Validation Failed" });
-        }
-      } catch (error) {
-        console.log("something went wrong", error);
+        });
+        console.log("email sent");
+      } else {
+        console.log("recaptchares", reCaptchaRes);
         return res
-          .status(error.statusCode || 500)
-          .json({ error: error.message });
+          .status(error.statusCode || 401)
+          .json({ error: "Captcha Validation Failed" });
       }
+
       console.log("I'm sending the success code");
       return res.status(200).json({ error: "" });
+    })
+    .catch((error) => {
+      console.log("something went wrong", error);
+      return res.status(error.statusCode || 500).json({ error: error.message });
     });
 }
 
