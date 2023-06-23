3a37a86b49785918cb410aba08cec840894a2f8b
Christopher Neisen <christopherjay71186@gmail.com>
2023-06-22 19:48:01 -0500

    minor tweaks to weclean content

--- src/pages/ourwork/weclean.tsx
+++ src/pages/ourwork/weclean.tsx
@@ -36,24 +36,12 @@ export default function WeClean() {
           <h2 className={styles.descriptionHeader}>Introduction</h2>
           <br />
           <p style={{ paddingLeft: 30 }}>
-            WeCleanLocal saw a need in the cleaning industry for an application
-            tailor-made for their unique administrative needs. Integrity Tech
-            was responsible for taking their design concept and turning it into
-            a functional SaaS app.
+            WeCleanLocal saw a demand in the cleaning industry for a tool to
+            assist with their administrative needs. Integrity Tech was
+            responsible for taking their design concept and turning it into a
+            functional SaaS app.
           </p>
-          <br />
-          <h2 className={styles.descriptionHeader}>Objective</h2>
-          <br />
-          <p style={{ paddingLeft: 30 }}>
-            Integrity Tech managed to take a design concept and not only
-            complete the development of the actual software, but also plan its
-            data architecture. The WeClean App is not only an administrative
-            tool. It also acts as a POS system, a marketing tool, and a customer
-            management tool. We developed all of these features while at the
-            same time making sure that the data was properly handled. With this
-            level of complexity, it's easy to introduce bugs which we took steps
-            to minimize at every turn.
-          </p>
+
           <br />
           <h2 className={styles.descriptionHeader}>Project's Main Tech</h2>
           <br />
@@ -81,8 +69,8 @@ export default function WeClean() {
           <h2 className={styles.descriptionHeader}>The Plan</h2>
           <br />
           <p style={{ paddingLeft: 30 }}>
-            The data structure of the WeClean App was relationally structured so
-            it became important to build the app in a specific sequence.
+            The data structure of the WeClean App was relationally structured,
+            so it became important to build the app in a specific sequence.
           </p>
           <br />
           <ul style={{ paddingLeft: 60 }}>
@@ -90,15 +78,15 @@ export default function WeClean() {
               <span style={{ fontWeight: 700, paddingRight: 3 }}>
                 Teir 1: Admin Portal
               </span>{" "}
-              The purpose of the admin portal was to give a way for the
-              WeCleanLocal staff to create and manage accounts.
+              This is to give a way for the WeCleanLocal staff to create and
+              manage accounts.
             </li>
             <li>
               <span style={{ fontWeight: 700, paddingRight: 3 }}>
                 Tier 2: Partner Account Settings
               </span>
-              The partner account settings allowed each account to have a fully
-              customized experience that fit their company needs.
+              This allows each account to have a fully customized experience
+              that fits their company needs.
             </li>
             <li>
               <span style={{ fontWeight: 700, paddingRight: 3 }}>
@@ -111,33 +99,35 @@ export default function WeClean() {
               <span style={{ fontWeight: 700, paddingRight: 3 }}>
                 Tier 4: Customers and Cleaners
               </span>
-              Being able to add customers and cleaners to be later attached to
-              bookings gives this application a full POS system
+              A full POS system allows customer and cleaner accounts to be added
+              to bookings.
             </li>
             <li>
               <span style={{ fontWeight: 700, paddingRight: 3 }}>
                 Tier 5: Coupons and Giftcards
               </span>
-              Partners can create promotions for their cleaning business right
-              from the dashboard.
+              Partners can create promotions for their cleaning business from
+              the dashboard.
             </li>
             <li>
               {" "}
               <span style={{ fontWeight: 700, paddingRight: 3 }}>
                 Tier 6: Bookings
               </span>
-              Bookings is where everything comes together. It's a cleaning order
-              form that utilizes all the previous tiers of data.
+              This detailed booking form utilizes all previous tiers of data.
             </li>
           </ul>
           <br />
           <h2 className={styles.descriptionHeader}>Conclusion</h2>
           <br />
           <p style={{ paddingLeft: 30 }}>
-            The WeClean SaaS App is an expanding project and additional features
-            are planned for the future. Integrity Tech's relationship with
-            WeCleanLocal remains strong, and we look forward to helping them
-            grow their application further in the future.
+            Integrity Tech completed the development of this software which
+            includes an administrative tool, a POS system, a marketing tool, and
+            a customer management tool. The WeClean SaaS App is an expanding
+            project and additional features are planned for the future.
+            Integrity Tech's relationship with WeCleanLocal remains strong, and
+            we look forward to helping them grow their application further in
+            the future.
           </p>
         </div>
       </div>
