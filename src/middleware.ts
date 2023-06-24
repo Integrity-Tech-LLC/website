0f581706e86c30fe0609b16634aeddd1f2f08f00
Christopher Neisen <christopherjay71186@gmail.com>
2023-06-24 11:38:50 -0500

    clean up

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,6 @@ import  { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 type Environment = "production" | "development" | "other";
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
     const currentEnv = process.env.NODE_ENV as Environment;
-    console.log(currentEnv, "currentEnv");
-    console.log(req.headers.get("x-forwarded-proto"), "req.headers")
 
     if (currentEnv === 'production' && 
          req.headers.get("x-forwarded-proto") !== "https,http") {
