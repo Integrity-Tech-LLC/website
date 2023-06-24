77dbcf1a27b6d040a1d6d5beaac995e59409b65f
Christopher Neisen <christopherjay71186@gmail.com>
2023-06-24 11:11:02 -0500

    lets see if we can get the whole url

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ type Environment = "production" | "development" | "other";
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
     const currentEnv = process.env.NODE_ENV as Environment;
     console.log(currentEnv, "currentEnv");
-    console.log(req.headers, "req.headers")
+    console.log(req.nextUrl, "req")
 
     if (currentEnv === 'production' && 
          req.headers.get("x-forwarded-proto")?.indexOf("https") !== -1) {
