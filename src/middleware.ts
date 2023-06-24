feda7a96444a6f008425bba32ca81fe3ea3c513f
Christopher Neisen <christopherjay71186@gmail.com>
2023-06-24 10:59:04 -0500

    troubleshooting https redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,11 @@ import  { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 type Environment = "production" | "development" | "other";
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
     const currentEnv = process.env.NODE_ENV as Environment;
+    console.log(currentEnv, "currentEnv");
+    console.log(req.headers.get("x-forwarded-proto"), "req.headers")
 
     if (currentEnv === 'production' && 
-        req.headers.get("x-forwarded-proto") !== "https" && req.headers.get("x-forwarded-proto")?.indexOf("https") !== -1) {
+         req.headers.get("x-forwarded-proto")?.indexOf("https") !== -1) {
         return NextResponse.redirect(
            `https://${req.headers.get('host')}${req.nextUrl.pathname}`,
            301
