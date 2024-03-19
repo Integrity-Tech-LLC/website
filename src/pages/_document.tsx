ce781f0aa62291fc18c40f8c73dc9228f438e958
Christopher Neisen <christopherjay71186@gmail.com>
2024-03-19 13:24:38 -0500

    after reviewing the commits, the head tag somehow got nested in the body, testing

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <Head>
+        <title>Integrity Tech</title>
+        <meta
+          name="description"
+          content="Website for software company Integrity Tech"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="icon_trimmed_transparent_blue.png"
+        />
+      </Head>
       <body>
-        <Head></Head>
         <Main />
         <NextScript />
       </body>
