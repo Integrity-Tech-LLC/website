4becf19961bf509f14951801870ce7819dbdd746
Christopher Neisen <christopherjay71186@gmail.com>
2024-03-28 10:23:36 -0500

    updated maintenance packages

--- src/pages/services/maintenance.tsx
+++ src/pages/services/maintenance.tsx
@@ -24,9 +24,9 @@ export default function Maintenance() {
             <br />
             <br />
             Note: All costs associated with additional third-party integrations
-            not listed in the agreed upon package are charged per cost of
-            service. All additional maintenance service hours outside of the
-            agreed upon plan are charged at $200 an hour.
+            not listed in the agreed upon package are charged per service. All
+            additional maintenance service hours outside of the agreed upon plan
+            are charged at $200 an hour.
           </p>
         </div>
       </div>
@@ -43,7 +43,7 @@ export default function Maintenance() {
       <div className={styles.container}>
         <h2 className={styles.header}>Basic Upkeep</h2>
         <ul className={styles.text}>
-          <li className={styles.priceMonth}>$100/month</li>
+          <li className={styles.priceMonth}>$200/month</li>
           <li>Free website monitoring</li>
           <li>Free basic hosting</li>
           <li>Free domain registration</li>
@@ -55,7 +55,7 @@ export default function Maintenance() {
       <div className={styles.container}>
         <h2 className={styles.header}>Moderate Upkeep</h2>
         <ul className={styles.text}>
-          <li className={styles.priceMonth}>$200/month</li>
+          <li className={styles.priceMonth}>$400/month</li>
           <li>Includes everything from Basic Upkeep</li>
           <li>Up to 5 email addresses on your Google Workspace account</li>
           <li>Up to 3 free service hours per month</li>
@@ -64,21 +64,12 @@ export default function Maintenance() {
       <div className={styles.container}>
         <h2 className={styles.header}>Consistent Upkeep</h2>
         <ul className={styles.text}>
-          <li className={styles.priceMonth}>$500/month</li>
+          <li className={styles.priceMonth}>$1000/month</li>
           <li>Includes Basic Upkeep and Moderate Upkeep</li>
           <li>Free Premium hosting</li>
           <li>Free Database hosting</li>
           <li>Up to 10 email addresses on your Google Workspace account</li>
-          <li>Up to 5 free service hours per month</li>
-        </ul>
-      </div>
-      <div className={styles.container}>
-        <h2 className={styles.header}>Full-Time Upkeep</h2>
-        <ul className={styles.text}>
-          <li className={styles.priceMonth}>$1000/month</li>
-          <li>Includes Basic Upkeep, Moderate Upkeep and Consistent Upkeep</li>
-          <li>Up to 25 email addresses on your Google Workspace account</li>
-          <li>Up to 12 free service hours per month</li>
+          <li>Up to 8 free service hours per month</li>
         </ul>
       </div>
     </>
